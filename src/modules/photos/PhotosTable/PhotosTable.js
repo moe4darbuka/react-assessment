0e6a05b97e7ae6ded5c72f4e246b88871528f265
Moe El Hariri <m.elhariri@newcoredigital.com>
2021-12-18 14:34:02 +0200

    add edit and delete actions

--- src/modules/photos/PhotosTable/PhotosTable.js
+++ src/modules/photos/PhotosTable/PhotosTable.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Paper from "@mui/material/Paper";
-import TableContainer from "@mui/material/TableContainer";
-import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableBody from "@mui/material/TableBody";
-import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
+import {
+  Paper,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TextField,
+  Button,
+} from "@mui/material";
 import Paginator from "../../../shared/paging/Paginator";
 import usePaging from "../../../shared/paging/usePaging";
 import { getPhotos } from "./photosApi";
@@ -16,10 +20,66 @@ const PhotosTable = () => {
   const { pageNumber, handlePageChange, getPageRows } = usePaging();
 
   useEffect(() => {
-    getPhotos().then((response) => setPhotos(response));
+    getPhotos().then((data) => {
+      const nextPhotos = data.map((photo) => ({
+        ...photo,
+        editing: false,
+        editedTitle: photo.title,
+        editedTitleError: false,
+      }));
+
+      setPhotos(nextPhotos);
+    });
   }, []);
 
   const pagePhotos = getPageRows(photos);
+
+  const handlePhotoEdit = (photoId) => {
+    const nextPhotos = photos.map((photo) =>
+      photo.id === photoId
+        ? {
+            ...photo,
+            editing: true,
+          }
+        : photo
+    );
+
+    setPhotos(nextPhotos);
+  };
+
+  const handlePhotoTitleChange = (photoId, e) => {
+    const nextPhotos = photos.map((photo) =>
+      photo.id === photoId
+        ? {
+            ...photo,
+            editedTitle: e.target.value,
+            editedTitleError: !e.target.value.trim(),
+          }
+        : photo
+    );
+
+    setPhotos(nextPhotos);
+  };
+
+  const handlePhotoEditDone = (photoId) => {
+    const nextPhotos = photos.map((photo) =>
+      photo.id === photoId
+        ? {
+            ...photo,
+            editing: false,
+            title: photo.editedTitle.trim(), // discard whitespace when committing the title
+            editedTitle: photo.editedTitle.trim(),
+          }
+        : photo
+    );
+
+    setPhotos(nextPhotos);
+  };
+
+  const handlePhotoDelete = (photoId) => {
+    const nextPhotos = photos.filter((photo) => photo.id !== photoId);
+    setPhotos(nextPhotos);
+  };
 
   return (
     <Paper className="table" sx={{ width: "66.6667%" }}>
@@ -27,24 +87,69 @@ const PhotosTable = () => {
         <Table stickyHeader={true}>
           <TableHead>
             <TableRow>
-              <TableCell width="20%">Id</TableCell>
-              <TableCell width="20%">Album Id</TableCell>
-              <TableCell width="40%">Title</TableCell>
-              <TableCell width="20%">Thumbnail</TableCell>
+              <TableCell>Id</TableCell>
+              <TableCell>Album Id</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Thumbnail</TableCell>
+              <TableCell width="150px">Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {pagePhotos.map((row) => (
-              <TableRow key={row.id}>
-                <TableCell>{row.id}</TableCell>
-                <TableCell>{row.albumId}</TableCell>
-                <TableCell>{row.title}</TableCell>
+            {pagePhotos.map((photo) => (
+              <TableRow key={photo.id}>
+                <TableCell>{photo.id}</TableCell>
+                <TableCell>{photo.albumId}</TableCell>
+                <TableCell>
+                  {!photo.editing && photo.title}
+                  {photo.editing && (
+                    <TextField
+                      autoFocus={true}
+                      fullWidth={true}
+                      size="small"
+                      value={photo.editedTitle}
+                      error={photo.editedTitleError}
+                      onChange={(e) => handlePhotoTitleChange(photo.id, e)}
+                    />
+                  )}
+                </TableCell>
                 <TableCell>
                   <img
                     className="photos-table__row-img"
-                    src={row.thumbnailUrl}
-                    alt={row.title}
+                    src={photo.thumbnailUrl}
+                    alt={photo.title}
                   />
+                </TableCell>
+                <TableCell>
+                  {!photo.editing && (
+                    <Button
+                      className="table__action-button"
+                      variant="contained"
+                      size="small"
+                      onClick={() => handlePhotoEdit(photo.id)}
+                    >
+                      Edit
+                    </Button>
+                  )}
+                  {photo.editing && (
+                    <Button
+                      className="table__action-button"
+                      variant="contained"
+                      size="small"
+                      color="success"
+                      onClick={() => handlePhotoEditDone(photo.id)}
+                    >
+                      Done
+                    </Button>
+                  )}
+                  <Button
+                    className="table__action-button"
+                    variant="outlined"
+                    size="small"
+                    color="error"
+                    onClick={() => handlePhotoDelete(photo.id)}
+                  >
+                    Delete
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
